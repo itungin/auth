92b3bcc70c736afa1fb639db4d65eb1720e59650
Wangjarim <157486911+Wangjarimm@users.noreply.github.com>
2024-12-06 18:54:49 +0700

    revisi

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function apiResponse(result){
         redirect("/testi");
     }else{
         setInner("content","Silahkan lakukan pendaftaran dahulu ke Itungin");
-        redirect("/");  
+        redirect("/login");  
        //redirect("https://wa.me/pamongdesa?text=bantuan+operator");
     }
 }
