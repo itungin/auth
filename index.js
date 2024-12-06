fb5b03b85c10baf8d26251764f5769ba06f63a15
Wangjarimm <karamissuu@gmail.com>
2024-12-06 21:04:33 +0700

    Disable redirect calls in responseFunction and apiResponse for debugging purposes

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.
 import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
-if (getCookie("login")===""){
-    redirect("/");
-}
+// if (getCookie("login")===""){
+//     // redirect("/");
+// }
 
 getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/user","login",getCookie("login"),responseFunction)
 
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Silahkan lakukan pendaftaran dahulu ke Itungin "+result.data.name);
-        redirect("/");
+        // redirect("/");
     }else{
         getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/konsumen/user","login",getCookie("login"),apiResponse)
     }
@@ -22,10 +22,11 @@ function responseFunction(result){
 function apiResponse(result){
     if (result.status===200){
         setInner("content","Selamat datang "+result.data.data.fullname);
-        redirect("/testi");
+        // redirect("/testi");
+        console.log(result);
     }else{
         setInner("content","Selamat Datang");
-        redirect("/dashboard");  
+        // redirect("/dashboard");  
        //redirect("https://wa.me/pamongdesa?text=bantuan+operator");
     }
 }
