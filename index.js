418a64c5aff2d56fc724f705ba83711a23ca4dc8
Wangjarimm <karamissuu@gmail.com>
2024-12-06 18:37:29 +0700

    Update redirect URLs in responseFunction and apiResponse for proper navigation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/user","
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Selamat datang "+result.data.name);
-        redirect("/dashboard");
+        redirect("https://itung.in.my.id/dashboard/");
     }else{
         getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/konsumen/user","login",getCookie("login"),apiResponse)
     }
@@ -25,7 +25,7 @@ function apiResponse(result){
         redirect("/testi");
     }else{
         setInner("content","Silahkan lakukan pendaftaran dahulu ke Itungin");
-        redirect("/login");  
+        redirect("https://itung.in.my.id/");  
        //redirect("https://wa.me/pamongdesa?text=bantuan+operator");
     }
 }
