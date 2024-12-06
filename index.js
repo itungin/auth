945778174616e9ec267656fa939692074ff5c73f
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-12-06 22:35:32 +0700

    Redirect

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
-import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
+// import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
 // if (getCookie("login")===""){
 //     // redirect("/");
@@ -12,7 +12,7 @@ getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/user","
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Silahkan lakukan pendaftaran dahulu ke Itungin "+result.data.name);
-        // redirect("/");
+        redirect("https://itung.in.my.id/");
     }else{
         getJSON("https://asia-southeast2-awangga.cloudfunctions.net/itungin/data/konsumen/user","login",getCookie("login"),apiResponse)
     }
@@ -22,7 +22,7 @@ function responseFunction(result){
 function apiResponse(result){
     if (result.status===200){
         setInner("content","Selamat datang "+result.data.data.fullname);
-        // redirect("/testi");
+        redirect("https://itung.in.my.id/dashboard/");
         console.log(result);
     }else{
         setInner("content","Selamat Datang");
